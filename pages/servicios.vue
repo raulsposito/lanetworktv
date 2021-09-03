10548ed55dde241bd44ab88dbaed53162090c774
raulsposito <raulsposito@gmail.com>
2021-09-02 22:40:33 -0300

    update services text content

--- pages/servicios.vue
+++ pages/servicios.vue
@@ -33,31 +33,42 @@
             <div class="text-lg max-w-prose mx-auto">
             <h1>
                 <span class="block text-base text-center text-brand font-semibold tracking-wide uppercase">Pioneros en</span>
-                <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Canal de tv online y streaming</span>
+                <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">TV online y streaming</span>
             </h1>
-            <p class="mt-8 text-xl text-gray-500 leading-8">Con la señal de tv usted puede emitir y grabar simultáneamente para todo el mundo. Esta señal audiovisual se podrá tanto ver en tiempo real con la posibilidad retroceder hasta cuatro horas en cada transmisión o verla luego de emitida la señal, cuantas veces lo desee.  A ésta transmisión, se le suma la posibilidad de interactuar mediante chat o de forma audiovisual, con el telespectador. Se puede transmitir 1, 2 o las cámaras que necesite, PIP o simultáneamente, en calidad 480P ( calidad DVD) 720P (half HD) o 1080P (full HD).</p>
+            <p class="mt-8 text-xl text-gray-500 leading-8">Somos Pioneros en transmitir a 3 cámaras simultáneamente en Uruguay, antes que los Canales de Tv 4, 5, 10 o 12. También somos Pioneros en transmitir en vivo hacia Internet desde un Vehículo en movimiento. Nuestros tiempos, nos piden interconexión entre personas mediante plataformas como Zoom, a varias cámaras y en calidad Full HD. Tenemos la posibilidad de emitir señales en vivo a varias plataformas simultáneamente, como Facebook Live, Youtube, Zoom, Twitch (justin.tv), Twitter, Linked In, Steam solo por nombrar las más conocidas, 33 plataformas diferentes. Las nuevas transmisiones o streaming, son a más de 3 cámaras en Full HD manejando software que permite tener cámaras Wi-Fi y Drones en las emisiones que se graban para dejar registro.</p>
             </div>
 
             <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
             <div class="text-lg max-w-prose mx-auto">
             <h1>
-                <span class="mt-10 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Gráfica</span>
+                <span class="mt-10 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Hardware</span>
             </h1>
-            <p class="mt-8 text-xl text-gray-500 leading-8">Siempre desde el arte proporcionando soluciones prácticas y accesibles logrando llegar a marcar pautas de consumo para qué su empresa o producto se logre el impacto deseado sobre el potencial consumidor.</p>
+            <p class="mt-8 text-xl text-gray-500 leading-8">Servicio de reparación de Hardware de Pc, Notebooks y tablets. Nuestro taller posee mas de 20 años de experiencia en la reparación de computadoras. Tenemos una amplia gama de posibilidades para darle una segunda vida a su Pc o notebook, que con el servicio adecuado al Software y Hardware se logran resultados con poca inversión. Cada Pc o notebook con el uso adecuado tienen una vida útil de 10 años o más.</p>
+            <p class="mt-8 text-xl text-gray-500 leading-8">Armado de Pc Gamer o Islas de Edición y computadoras en general a medida del cliente. Porque lo importante es saber el uso que se le va a dar a la computadora para poder armar el mejor equipo posible con el dinero que disponga para la inversión. Esto lo realizo por la conjunción de mis conocimientos de Ingeniería y Tec. en Pc.</p>
+            <p class="mt-8 text-xl text-gray-500 leading-8">Respaldo de Discos Duros. Este importante detalle puede ser crucial ante la falla de los mismos. Se realizan en el lugar o remotamente según la necesidad y volumen del cliente. Se realizan en DVD o Blu-Ray o SSD.</p>
+            <p class="mt-8 text-xl text-gray-500 leading-8">Instalación de sistemas de Video Vigilancia. Más de 10 años de experiencia y confiabilidad, en nuestro soporte a los sistemas que instalamos, como a los que no. Realizamos actualización y reparación de DVRs, Cámaras IP y Wi-fi. También servicio remoto a través de internet móvil ya sea en Montevideo o en el Interior. Programación de servidores que graban y monitorizan varias cámaras en diferentes puntos del país. La tecnología para respaldar remotamente lo que sucede en cualquier parte del mundo, en tiempo real. Integración e/o instalación con alarmas. Programación personalizada de alarmas vía E-Mail a tu celular. Uno configura las cámaras que si hay movimiento te avisan, si se corta la luz, te avisa, todo remotamente. Utilización de la mas alta tecnología disponible en el mercado.</p>
+            </div>
+            <div class="text-lg max-w-prose mx-auto">
+            <h1>
+                <span class="mt-10 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Software</span>
+            </h1>
+            <p class="mt-8 text-xl text-gray-500 leading-8">Instalación de Sistemas operativos Linux o Windows en sus diferentes versiones. Instalación de software de Edición de Video, Audio, Diseño Gráfico, Redes Sociales y demás aditamentos para Youtubers/Social Media Managers. Software de oficina clásicos y antivirus Gratis y eficaces, con versiones pro pagas para profesionales que incluyen, Cyber seguridad y cobertura de E-comerce, ante el haqueo de tus cuentas de E-mail y/o redes Sociales. Programación de redes de Wi-Fi anti-haqueo de vecinos que se "cuelgan" del Internet impidiendo el tráfico fluido del mismo.</p>
+            <p class="mt-8 text-xl text-gray-500 leading-8">Servicio de escaneo de Diapositivas o Negativos y fotografías con máxima calidad, hasta 9600 PPP, en formato JPG, PGA,TIFF de hasta 600 Mb de peso, imagen sin compresión para gigantografía e impresión . De ser necesario se puede realizar reconstrucción de daños de las mismas con edición en PhotoShop y programación especial del Scaner HP Scanjet.</p>
+            <p class="mt-8 text-xl text-gray-500 leading-8">Eventos y congresos. Proyección de imágenes de hasta 200' en paredes o pantalla de 1,5 metros. Calidad Full HD 1080P, 3200 lúmenes, conexión HDMI, VGA o RCA, entrada de audio y parlante. Amplificación de voz para eventos con micrófonos inalámbricos. Integración con transmisiones vía Zoom e interacción con los visores vía Zoom. Emisiones a Plataformas y redes Sociales.</p>
             </div>
 
             <div class="text-lg max-w-prose mx-auto">
             <h1>
                 <span class="mt-10 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Fotografía</span>
             </h1>
-            <p class="mt-8 text-xl text-gray-500 leading-8">Servicio fotográfico para eventos: los años de experiencia sumados a los equipos de primera, hacen un registro preciso para la captura de esos momentos fundamentales, dejándolos  indelebles en el tiempo.</p>
+            <p class="mt-8 text-xl text-gray-500 leading-8">Servicio fotográfico para eventos: los años de experiencia sumados a los equipos de primera, hacen un registro preciso para la captura de esos momentos fundamentales, dejándolos indelebles en el tiempo. Con el escaneo de diapositivas o negativos, usted recupera el pasado en una calidad de hasta 9600 ppp, listo para imprimir hasta en gigantografía. También escaneo de imágenes y recuperación por daños por el tiempo.</p>
             </div>
 
             <div class="text-lg max-w-prose mx-auto">
             <h1>
                 <span class="mt-10 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Producciones Audiovisuales</span>
             </h1>
-            <p class="mt-8 text-xl text-gray-500 leading-8">Un equipo de trabajo con amplia capacidad de adaptación al producto a registrar y/o editar, sumándole el conocimiento adquirido para brindarle el más alto nivel de contenido audiovisual tanto sea en el Teatro Solís o grabar un partido de baby fútbol.</p>
+            <p class="mt-8 text-xl text-gray-500 leading-8">Un equipo de trabajo con amplia experiencia y confiabilidad, que posee una gran capacidad adaptación a los contratiempos al transmitir y/o editar. Sumándole el conocimiento adquirido para brindarle el más alto nivel de contenido audiovisual tanto sea en el Teatro Solís o grabar un evento particular. En cuanto a los equipos de trabajo son de última generación adaptándonos a la norma actual 4K. Nuestros trabajos y clientes hablan por nosotros.</p>
             </div>
 
             <figure>
